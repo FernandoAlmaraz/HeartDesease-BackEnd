b1c7bb4f42b47a2c01900ae3121b610835a841a2
FernandoAlmaraz <fernandoalmarazdelaquintana@gmail.com>
2024-02-12 18:16:39 -0400

    Initial commit

---
+++ app/Http/Controllers/ConsultController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Consult;
+use Illuminate\Http\Request;
+
+class ConsultController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Consult $consult)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Consult $consult)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Consult $consult)
+    {
+        //
+    }
+}
